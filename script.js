dc46fc943e9733fffaa6794359e1ddc2a69f84ff
Jonny Hall <car87maniac@googlemail.com>
2022-09-06 19:24:29 +0100

    got API request & fetch working (not WIKI yet)

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //alert("hello");
+fetch(
+  "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=45965e86278e1d1806a35a380a87eeea"
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
