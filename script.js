24314ec336693822aac567949bc29c176bd18bcd
Jonny Hall <car87maniac@googlemail.com>
2022-09-07 00:45:08 +0100

    got API request & fetch working (not WIKI yet)

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 //alert("hello");
+// fetch(
+//   "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=45965e86278e1d1806a35a380a87eeea"
+// )
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// fetch(
+//   "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche",
+//   {
+//     mode: "cors",
+//   }
+// );
+
 fetch(
-  "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=45965e86278e1d1806a35a380a87eeea"
+  "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche"
 )
   .then((response) => response.json())
   .then((data) => console.log(data));
