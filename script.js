b186938669a4f1e3adcbe4c68a99ad91b4db16a1
Jonny Hall <car87maniac@googlemail.com>
2022-09-07 23:07:31 +0100

    Wiki - got key now working out a post request

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //alert("hello");
 // fetch(
 //   "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=45965e86278e1d1806a35a380a87eeea"
-// )..................................................../*My API key - 45965e86278e1d1806a35a380a87eeea*/
+// )..................................................../*My API key - 45965e86278e1d1806a35a380a87eeea (Would hide it but its free)*/
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
 
 // fetch(
 //   "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche",
-//   //.................................../*My API key -   hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ*/
+//   //.................................../*My API key -   hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ (Would hide it but its free)*/
 //   {
 //     mode: "cors",
 //   }
@@ -19,6 +19,7 @@
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
 
+/*
 const img = document.querySelector("img");
 fetch(
   "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche"
@@ -30,3 +31,20 @@ fetch(
     console.log(response.data.images.original.url);
     img.src = response.data.images.original.url;
   });
+*/
+
+fetch("https://meta.wikimedia.org/w/rest.php/oauth2/access_token", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "something",
+    body: "else",
+    userId: 1,
+  }),
+  headers: {
+    "content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+// curl -X POST -F 'grant_type=client_credentials'
