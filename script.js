8f6356c72db1c15464d4abaf84217dd976bd1777
Jonny Hall <car87maniac@googlemail.com>
2022-09-08 06:42:27 +0100

    Wiki - got Keys, working out POST request

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //alert("hello");
+
+// //WORKING retreive object from openWeatherMAP
 // fetch(
 //   "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=45965e86278e1d1806a35a380a87eeea"
-// )..................................................../*My API key - 45965e86278e1d1806a35a380a87eeea (Would hide it but its free)*/
+// ) //..................................................../*My API key - 45965e86278e1d1806a35a380a87eeea (Would hide it but its free)*/
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
 
@@ -15,11 +17,12 @@
 
 // fetch(
 //   "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche"
-// )//https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=dog
+// ) //https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=dog
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
 
-/*
+/***************
+//WORKING random GIF from gify
 const img = document.querySelector("img");
 fetch(
   "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche"
@@ -31,8 +34,11 @@ fetch(
     console.log(response.data.images.original.url);
     img.src = response.data.images.original.url;
   });
-*/
+  *********************/
 
+/***** 
+Post request using fetch
+syntax from - https://www.geeksforgeeks.org/get-and-post-method-using-fetch-api/
 fetch("https://meta.wikimedia.org/w/rest.php/oauth2/access_token", {
   method: "POST",
   body: JSON.stringify({
@@ -47,4 +53,17 @@ fetch("https://meta.wikimedia.org/w/rest.php/oauth2/access_token", {
   .then((response) => response.json())
   .then((json) => console.log(json));
 
-// curl -X POST -F 'grant_type=client_credentials'
+*****/
+
+/*************
+curl -X POST -F 'grant_type=Jonny5800' \
+-F 'client_id=YOUR_CLIENT_ID' \
+-F 'client_secret=YOUR_CLIENT_SECRET' \
+https://meta.wikimedia.org/w/rest.php/oauth2/access_token
+*****************/
+
+//curl -v -H "Content-Type: application/json" -X POST -d '{"name":"your name","phonenumber":"111-111"}' http://www.abc.com/details
+
+fetch("https://meta.wikimedia.org/w/rest.php/oauth2/access_token")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
