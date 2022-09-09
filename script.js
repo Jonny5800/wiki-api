7a67c5a0073216856df6bc3204478c4e2de364ac
Jonny Hall <car87maniac@googlemail.com>
2022-09-09 11:15:33 +0100

    inputs to paragraph and console on click

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-//alert("hello");
+const txt1 = document.getElementById("tbuser");
+const btn1 = document.getElementById("btn1");
+const out1 = document.getElementById("output1");
 
-// //WORKING retreive object from openWeatherMAP
-// fetch(
-//   "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=45965e86278e1d1806a35a380a87eeea"
-// ) //..................................................../*My API key - 45965e86278e1d1806a35a380a87eeea (Would hide it but its free)*/
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+function fun1() {
+  out1.innerHTML = txt1.value;
+}
 
-// fetch(
-//   "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche",
-//   //.................................../*My API key -   hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ (Would hide it but its free)*/
-//   {
-//     mode: "cors",
-//   }
-// );
+btn1.addEventListener("click", fun1, () => {
+  console.log(txt1);
+});
 
-// fetch(
-//   "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche"
-// ) //https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=dog
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+const userInput = document.getElementById("search-bar");
+console.log(userInput);
 
+document.getElementById("search-bar").addEventListener("click", () => {
+  console.log(userInput.value);
+});
+
+//<input type="button" value="submit" onclick="send()" />;
+let uname = document.querySelector("#uname");
+function send() {
+  console.log(uname.value);
+}
+// document
+//   .getElementById("search-button")
+//   .addEventListener("submit", (e) => e.preventDefault());
+// console.log(userInput.value);
 /***************
 //WORKING random GIF from gify
 const img = document.querySelector("img");
@@ -34,7 +39,103 @@ fetch(
     console.log(response.data.images.original.url);
     img.src = response.data.images.original.url;
   });
-  *********************/
+/*********************/
+//WORKING retreive object from openWeatherMAP
+// fetch(
+//   "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=45965e86278e1d1806a35a380a87eeea"
+// ) //..................................................../*My API key - 45965e86278e1d1806a35a380a87eeea (Would hide it but its free)*/
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+/*
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+*/
+// fetch(
+//   "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche",
+//   //.................................../*My API key -   hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ (Would hide it but its free)*/
+//   {
+//     mode: "cors",
+//   }
+// );
+
+// fetch(
+//   "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=porsche"
+// ) //https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=dog
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
 
 /***** 
 Post request using fetch
@@ -64,6 +165,11 @@ https://meta.wikimedia.org/w/rest.php/oauth2/access_token
 
 //curl -v -H "Content-Type: application/json" -X POST -d '{"name":"your name","phonenumber":"111-111"}' http://www.abc.com/details
 
+/**********************
 fetch("https://meta.wikimedia.org/w/rest.php/oauth2/access_token")
   .then((response) => response.json())
   .then((data) => console.log(data));
+************/
+
+// Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0YzZmODkzNjgwYWY4NzgwOGIyMWY5N2ViNDZhZTZhMyIsImp0aSI6ImExNzAzMjU0Y2RiNjFmM2UyOTUzZTUyMGYxMWJjZTUzNjkyZGMxZmY0MzYyZDAwNzlkZTkyODgzZWYzZWNmZWM4MGU4ODRiNjVmMjdhYmIxIiwiaWF0IjoxNjYyNjY0NTkxLCJuYmYiOjE2NjI2NjQ1OTEsImV4cCI6MzMyMTk1NzMzOTEsInN1YiI6IjcwNTc5NzYzIiwiaXNzIjoiaHR0cHM6XC9cL21ldGEud2lraW1lZGlhLm9yZyIsInJhdGVsaW1pdCI6eyJyZXF1ZXN0c19wZXJfdW5pdCI6NTAwMCwidW5pdCI6IkhPVVIifSwic2NvcGVzIjpbImJhc2ljIiwiY3JlYXRlZWRpdG1vdmVwYWdlIiwiZWRpdHByb3RlY3RlZCJdfQ.am4KjtLn97Jas7H6VOPVpEFp_9hmfytfa7Ypn2LTEdBknVg3quFdGXNgZVQtP4S5BPeD1Y4uiw9WZ-h0oltpyZYavONCKWr952XR34rvysLscPwBov3tauLbjKp7-46XSa41mJ2Y3hH6-jfQNIQ828Z5mzjnVMXiwZJz2hufIHfwdlD8yGYMSaWEEgh6DCT3ymUNciZOO8dmiP5t5KI-SThLsUhl0JJKHTXQySbEMAjTzRYwtFbkjaSMrkTME3F7mp8v1NYpCx6xH23x0P50V-geEdTg6XybJ_sSwhHQOi6xZut3jHmclJCqpVVeLBpyvZ6YcM2yCrpimTbzWzYH0MCh9jUP8UgEratfyC6qM9fBbVIR80GsUsKebmRwzRJO--LL31EG34xmiOdRPhzCahD1JUrjNmbYrEyXT8rQQJlzYIJiFXTarVtmQxvqA7Y131XJszgizBpG9rCCSp9p-tZP4NG3o7Arac6qFPQU0YnRMAA1qeb_5Nan3JW0vymLXupW_zxQSp73dUZx4LLBKV37lJbAF0JnM13V3JIVKjSYmIiwYHMTOAWKkI5dEYqVIjy5xmXRhs43SxZ6Lzo8j9kPYXuFP4oBrR4mxWhqMnY1KLDx90dTcW6eSCbqK5j8PV82mjNp0JiWZkeMvsiUzJ5x-TKl16meEdmsaOYylDM
+// https://api.wikimedia.org/core/v1/wikipedia/en/page/Earth/bare
