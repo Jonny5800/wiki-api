786dc293caad404da044cfc202f06d808d6614b9
Jonny Hall <car87maniac@googlemail.com>
2022-09-28 06:31:43 +0100

    Dropdown menu works with typed search

--- script.js
+++ script.js
@@ -17,21 +17,14 @@ const cityTemp = document.getElementById("city-temp");
 const cityCloudCover = document.getElementById("city-cloud-cover");
 const imageVariable = document.querySelector("img");
 
-function logCountry() {
-  var select = document.getElementById("country-list");
-  const trying = select.options[select.selectedIndex].text;
-  console.log("the selected value is " + trying);
-}
-
 userInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     searcher.click();
-    var text = select.options[select.selectedIndex].text;
-    console.log(text);
+    // var text = select.options[select.selectedIndex].text;
+    // console.log(text);
   }
 });
 
-//const dropSelection = document.getElementById("myDropdown").classList.toggle;
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -52,12 +45,20 @@ function invokeGifSearch() {
       imageVariable.src = response.data.images.original.url;
     });
 }
+/*THE "selectedCountry" MUST GO IN "FETHCSTRING" BY TEMPLATE LITERAL*/
+let selectedCountry = "";
+function logCountry() {
+  const select = document.getElementById("country-list");
+  selectedCountry = select.options[select.selectedIndex].text;
+  console.log("the selected value is " + selectedCountry);
+  //return selectedCountry;
+}
 function fetchWeather() {
   let location = userInput.value;
   console.log(location + "...location");
-  let fetchString = `http://api.openweathermap.org/data/2.5/weather?q= ${location} ,uk&APPID=45965e86278e1d1806a35a380a87eeea`;
-
-  fetch(fetchString)
+  let fetchString1 = `http://api.openweathermap.org/data/2.5/weather?q= ${location},uk&APPID=45965e86278e1d1806a35a380a87eeea`;
+  let fetchString2 = `http://api.openweathermap.org/data/2.5/weather?q= ${location},${selectedCountry}&APPID=45965e86278e1d1806a35a380a87eeea`;
+  fetch(fetchString2)
     .then((response) => response.json())
 
     .then(function (data) {
@@ -77,8 +78,8 @@ function fetchWeather() {
 
       cityCloudCover.innerHTML = "Cloud cover is " + data.clouds.all + "%";
       console.log("cloud cover " + data.clouds.all + "%");
-    });
-  /*.catch(function () {
+    })
+    .catch(function () {
       console.log("this prints when city not found");
       let errorGifSearch =
         "https://api.giphy.com/v1/gifs/translate?api_key=hwRcQwF2Fc2MiIwqGFOrXv9a2KI2ECXQ&s=" +
@@ -89,11 +90,11 @@ function fetchWeather() {
         })
         .then(function (response) {
           imageVariable.src = response.data.images.original.url;
-          cityWind.innerHTML = "Try";
-          cityTemp.innerHTML = "Searching again";
-          cityCloudCover.innerHTML = "(Maybe a typo)";
+          cityWind.innerHTML = "Try again";
+          cityTemp.innerHTML = "Maybe a typo";
+          cityCloudCover.innerHTML = "or country format not recognised";
         });
-    });*/
+    });
 }
 
 const countryList = [
@@ -102,247 +103,6 @@ const countryList = [
   "Albania",
   "Algeria",
   "American Samoa",
-  "Andorra",
-  "Angola",
-  "Anguilla",
-  "Antarctica",
-  "Antigua and Barbuda",
-  "Argentina",
-  "Armenia",
-  "Aruba",
-  "Australia",
-  "Austria",
-  "Azerbaijan",
-  "Bahamas (the)",
-  "Bahrain",
-  "Bangladesh",
-  "Barbados",
-  "Belarus",
-  "Belgium",
-  "Belize",
-  "Benin",
-  "Bermuda",
-  "Bhutan",
-  "Bolivia (Plurinational State of)",
-  "Bonaire, Sint Eustatius and Saba",
-  "Bosnia and Herzegovina",
-  "Botswana",
-  "Bouvet Island",
-  "Brazil",
-  "British Indian Ocean Territory (the)",
-  "Brunei Darussalam",
-  "Bulgaria",
-  "Burkina Faso",
-  "Burundi",
-  "Cabo Verde",
-  "Cambodia",
-  "Cameroon",
-  "Canada",
-  "Cayman Islands (the)",
-  "Central African Republic (the)",
-  "Chad",
-  "Chile",
-  "China",
-  "Christmas Island",
-  "Cocos (Keeling) Islands (the)",
-  "Colombia",
-  "Comoros (the)",
-  "Congo (the Democratic Republic of the)",
-  "Congo (the)",
-  "Cook Islands (the)",
-  "Costa Rica",
-  "Croatia",
-  "Cuba",
-  "Curaçao",
-  "Cyprus",
-  "Czechia",
-  "Côte d'Ivoire",
-  "Denmark",
-  "Djibouti",
-  "Dominica",
-  "Dominican Republic (the)",
-  "Ecuador",
-  "Egypt",
-  "El Salvador",
-  "Equatorial Guinea",
-  "Eritrea",
-  "Estonia",
-  "Eswatini",
-  "Ethiopia",
-  "Falkland Islands (the) [Malvinas]",
-  "Faroe Islands (the)",
-  "Fiji",
-  "Finland",
-  "France",
-  "French Guiana",
-  "French Polynesia",
-  "French Southern Territories (the)",
-  "Gabon",
-  "Gambia (the)",
-  "Georgia",
-  "Germany",
-  "Ghana",
-  "Gibraltar",
-  "Greece",
-  "Greenland",
-  "Grenada",
-  "Guadeloupe",
-  "Guam",
-  "Guatemala",
-  "Guernsey",
-  "Guinea",
-  "Guinea-Bissau",
-  "Guyana",
-  "Haiti",
-  "Heard Island and McDonald Islands",
-  "Holy See (the)",
-  "Honduras",
-  "Hong Kong",
-  "Hungary",
-  "Iceland",
-  "India",
-  "Indonesia",
-  "Iran (Islamic Republic of)",
-  "Iraq",
-  "Ireland",
-  "Isle of Man",
-  "Israel",
-  "Italy",
-  "Jamaica",
-  "Japan",
-  "Jersey",
-  "Jordan",
-  "Kazakhstan",
-  "Kenya",
-  "Kiribati",
-  "Korea (the Democratic People's Republic of)",
-  "Korea (the Republic of)",
-  "Kuwait",
-  "Kyrgyzstan",
-  "Lao People's Democratic Republic (the)",
-  "Latvia",
-  "Lebanon",
-  "Lesotho",
-  "Liberia",
-  "Libya",
-  "Liechtenstein",
-  "Lithuania",
-  "Luxembourg",
-  "Macao",
-  "Madagascar",
-  "Malawi",
-  "Malaysia",
-  "Maldives",
-  "Mali",
-  "Malta",
-  "Marshall Islands (the)",
-  "Martinique",
-  "Mauritania",
-  "Mauritius",
-  "Mayotte",
-  "Mexico",
-  "Micronesia (Federated States of)",
-  "Moldova (the Republic of)",
-  "Monaco",
-  "Mongolia",
-  "Montenegro",
-  "Montserrat",
-  "Morocco",
-  "Mozambique",
-  "Myanmar",
-  "Namibia",
-  "Nauru",
-  "Nepal",
-  "Netherlands (the)",
-  "New Caledonia",
-  "New Zealand",
-  "Nicaragua",
-  "Niger (the)",
-  "Nigeria",
-  "Niue",
-  "Norfolk Island",
-  "Northern Mariana Islands (the)",
-  "Norway",
-  "Oman",
-  "Pakistan",
-  "Palau",
-  "Palestine, State of",
-  "Panama",
-  "Papua New Guinea",
-  "Paraguay",
-  "Peru",
-  "Philippines (the)",
-  "Pitcairn",
-  "Poland",
-  "Portugal",
-  "Puerto Rico",
-  "Qatar",
-  "Republic of North Macedonia",
-  "Romania",
-  "Russian Federation (the)",
-  "Rwanda",
-  "Réunion",
-  "Saint Barthélemy",
-  "Saint Helena, Ascension and Tristan da Cunha",
-  "Saint Kitts and Nevis",
-  "Saint Lucia",
-  "Saint Martin (French part)",
-  "Saint Pierre and Miquelon",
-  "Saint Vincent and the Grenadines",
-  "Samoa",
-  "San Marino",
-  "Sao Tome and Principe",
-  "Saudi Arabia",
-  "Senegal",
-  "Serbia",
-  "Seychelles",
-  "Sierra Leone",
-  "Singapore",
-  "Sint Maarten (Dutch part)",
-  "Slovakia",
-  "Slovenia",
-  "Solomon Islands",
-  "Somalia",
-  "South Africa",
-  "South Georgia and the South Sandwich Islands",
-  "South Sudan",
-  "Spain",
-  "Sri Lanka",
-  "Sudan (the)",
-  "Suriname",
-  "Svalbard and Jan Mayen",
-  "Sweden",
-  "Switzerland",
-  "Syrian Arab Republic",
-  "Taiwan (Province of China)",
-  "Tajikistan",
-  "Tanzania, United Republic of",
-  "Thailand",
-  "Timor-Leste",
-  "Togo",
-  "Tokelau",
-  "Tonga",
-  "Trinidad and Tobago",
-  "Tunisia",
-  "Turkey",
-  "Turkmenistan",
-  "Turks and Caicos Islands (the)",
-  "Tuvalu",
-  "Uganda",
-  "Ukraine",
-  "United Arab Emirates (the)",
-  "United Kingdom of Great Britain and Northern Ireland (the)",
-  "United States Minor Outlying Islands (the)",
-  "United States of America (the)",
-  "Uruguay",
-  "Uzbekistan",
-  "Vanuatu",
-  "Venezuela (Bolivarian Republic of)",
-  "Viet Nam",
-  "Virgin Islands (British)",
-  "Virgin Islands (U.S.)",
-  "Wallis and Futuna",
-  "Western Sahara",
   "Yemen",
   "Zambia",
   "Zimbabwe",
